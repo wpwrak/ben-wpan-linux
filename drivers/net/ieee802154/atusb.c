c4435d5931a0ea95707055764a5d1d5d8da155e6
Werner Almesberger <werner@almesberger.net>
2013-04-01 01:12:58 -0300

    atusb.c: move definitions shared with firmware to atusb.h

--- drivers/net/ieee802154/atusb.c
+++ drivers/net/ieee802154/atusb.c
@@ -49,14 +49,10 @@
 #include <net/wpan-phy.h>
 
 #include "at86rf230.h"
+#include "atusb.h"
 
-
-#define ATUSB_VENDOR_ID	0x20b7	/* Qi Hardware*/
-#define ATUSB_PRODUCT_ID 0x1540	/* ATUSB */
 
 #define	JEDEC_ATMEL	0x1f	/* JEDEC manufacturer ID */
-
-#define ATUSB_BUILD_SIZE 256	/* maximum build version/date message length */
 
 #define	NUM_RX_URBS	4	/* allow for a bit of local latency */
 
@@ -70,66 +66,6 @@ struct atusb {
 	struct semaphore tx_sem;
 	struct completion tx_complete;
 };
-
-/* Commands to our device. Make sure this is synced with the firmware */
-enum atusb_requests {
-	ATUSB_ID			= 0x00,	/* system status/control grp */
-	ATUSB_BUILD,
-	ATUSB_RESET,
-	ATUSB_RF_RESET			= 0x10,	/* debug/test group */
-	ATUSB_POLL_INT,
-	ATUSB_TEST,			/* atusb-sil only */
-	ATUSB_TIMER,
-	ATUSB_GPIO,
-	ATUSB_SLP_TR,
-	ATUSB_GPIO_CLEANUP,
-	ATUSB_REG_WRITE			= 0x20,	/* transceiver group */
-	ATUSB_REG_READ,
-	ATUSB_BUF_WRITE,
-	ATUSB_BUF_READ,
-	ATUSB_SRAM_WRITE,
-	ATUSB_SRAM_READ,
-	ATUSB_SPI_WRITE			= 0x30,	/* SPI group */
-	ATUSB_SPI_READ1,
-	ATUSB_SPI_READ2,
-	ATUSB_SPI_WRITE2_SYNC,
-	ATUSB_RX_MODE			= 0x40, /* HardMAC group */
-	ATUSB_TX,
-};
-
-/*
- * Direction	bRequest		wValue		wIndex	wLength
- *
- * ->host	ATUSB_ID		-		-	3
- * ->host	ATUSB_BUILD		-		-	#bytes
- * host->	ATUSB_RESET		-		-	0
- *
- * host->	ATUSB_RF_RESET		-		-	0
- * ->host	ATUSB_POLL_INT		-		-	1
- * host->	ATUSB_TEST		-		-	0
- * ->host	ATUSB_TIMER		-		-	#bytes (6)
- * ->host	ATUSB_GPIO		dir+data	mask+p#	3
- * host->	ATUSB_SLP_TR		-		-	0
- * host->	ATUSB_GPIO_CLEANUP	-		-	0
- *
- * host->	ATUSB_REG_WRITE		value		addr	0
- * ->host	ATUSB_REG_READ		-		addr	1
- * host->	ATUSB_BUF_WRITE		-		-	#bytes
- * ->host	ATUSB_BUF_READ		-		-	#bytes
- * host->	ATUSB_SRAM_WRITE	-		addr	#bytes
- * ->host	ATUSB_SRAM_READ		-		addr	#bytes
- *
- * host->	ATUSB_SPI_WRITE		byte0		byte1	#bytes
- * ->host	ATUSB_SPI_READ1		byte0		-	#bytes
- * ->host	ATUSB_SPI_READ2		byte0		byte1	#bytes
- * ->host	ATUSB_SPI_WRITE2_SYNC	byte0		byte1	0/1
- *
- * host->	ATUSB_RX_MODE		on		-	0
- * host->	ATUSB_TX		flags		0	#bytes
- */
-
-#define ATUSB_FROM_DEV (USB_TYPE_VENDOR | USB_DIR_IN)
-#define ATUSB_TO_DEV (USB_TYPE_VENDOR | USB_DIR_OUT)
 
 
 /* ----- USB commands without data ----------------------------------------- */
