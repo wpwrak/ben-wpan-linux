a3bc9b46b6022c9ce13740ec0ed213be4b05c9db
Werner Almesberger <werner@almesberger.net>
2013-04-10 05:37:36 -0300

    spi-gpio-atben.c: replace gpio_{get,set}_value with optimized macros

--- drivers/spi/spi-gpio-atben.c
+++ drivers/spi/spi-gpio-atben.c
@@ -8,8 +8,36 @@
  * as published by the Free Software Foundation.
  */
 
+/*
+ * Replace gpio_{get,set}_value with optimized macros.
+ */
+
+#define __ASM_MACH_GENERIC_GPIO_H
+
+#define	PORT_BASE(gpio)	\
+	((void __iomem *) KSEG1ADDR(JZ4740_GPIO_BASE_ADDR+0x100*((gpio) >> 5)))
+#define	GPIO_MASK(gpio)	(1 << ((gpio) & 31))
+
+#define	gpio_get_value(gpio) \
+	(readl(PORT_BASE(gpio)) & GPIO_MASK(gpio))
+#define	gpio_set_value(gpio, value) \
+	writel(GPIO_MASK(gpio), \
+	    (value) ? PORT_BASE(gpio)+0x14 : PORT_BASE(gpio)+0x18)
+
+/* from mach-generic/gpio.h */
+
+#define gpio_cansleep	__gpio_cansleep
+int gpio_to_irq(unsigned gpio);
+int irq_to_gpio(unsigned irq);
+#include <asm-generic/gpio.h>		/* cansleep wrappers */
+
+#include <asm/mach-jz4740/base.h>
 #include <asm/mach-jz4740/gpio.h>
 
+
+/*
+ * Make specialized SPI-GPIO with inlined GPIO register access.
+ */
 
 #define	DRIVER_NAME	"spi_gpio_atben"
 
